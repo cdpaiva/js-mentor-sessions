2c83c9f0161af4aee520093f5b5917bfc768d94d
cdpaiva <cdpaivaa@gmail.com>
2026-01-24 09:45:14 -0500

    feat: add sorting

--- sof-languages/src/App.js
+++ sof-languages/src/App.js
@@ -13,6 +13,7 @@ function getInitialLangState() {
 function App() {
     const [languages, setLanguages] = useState(langList);
     const [newLanguage, setNewLanguage] = useState(getInitialLangState);
+    const [sortKey, setSortKey] = useState("admired");
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -39,9 +40,55 @@ function App() {
             setNewLanguage(getInitialLangState());
         }
     };
+
+    const handleSort = () => {
+        const sortedLanguages = languages.toSorted((a, b) => {
+            if (a[sortKey] > b[sortKey]) return -1;
+            if (a[sortKey] < b[sortKey]) return 1;
+            return 0;
+        });
+        setLanguages(sortedLanguages);
+    };
+
+    const handleKeySelect = (e) => {
+        setSortKey(e.target.value);
+    };
+
     return (
         <div className="container">
             <h1>Admired and desired programming languages</h1>
+            <div>
+                <div className="sort-radios">
+                    <input
+                        type="radio"
+                        id="name"
+                        name="sortKey"
+                        value={"name"}
+                        checked={sortKey === "name"}
+                        onChange={handleKeySelect}
+                    ></input>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="radio"
+                        id="admired"
+                        name="sortKey"
+                        value={"admired"}
+                        checked={sortKey === "admired"}
+                        onChange={handleKeySelect}
+                    ></input>
+                    <label htmlFor="admired">Admired</label>
+                    <input
+                        type="radio"
+                        id="desired"
+                        name="sortKey"
+                        value={"desired"}
+                        checked={sortKey === "desired"}
+                        onChange={handleKeySelect}
+                    ></input>
+                    <label htmlFor="desired">Desired</label>
+                </div>
+                <button onClick={handleSort}>Sort</button>
+            </div>
             <table className="lang-table">
                 <thead>
                     <tr>
